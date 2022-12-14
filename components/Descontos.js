e896b3dd11ac711d3f155eb46cc2d3e52ae12ca1
Helder Fonseca <helder2391@gmail.com>
2022-12-14 14:38:05 -0100

    add about info

--- components/Descontos.js
+++ components/Descontos.js
@@ -4,7 +4,7 @@ const Descontos = () => {
     return (
         <div className="mt-14 md:mt-20 mx-auto text-center">
             <h2 className="md:text-5xl text-4xl pb-3 font-bold"> Get discounts and become different! </h2> 
-            <p className="font-medium p-3"> Online requires the search for new strategies and models  </p>
+            <p className="font-medium p-3"> Being online requires a search for new digital strategies and models </p>
             <p className="font-medium pt-0.5 pb-0.5 pr-3 pl-3 bg-[#FBED04] inline rounded-md"> Choose the best plan for your company!  </p> 
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Descontos from '../components/Descontos'
-import Form from '../components/Form'
 import Header from '../components/Header'
-import Testemunhos from '../components/Testemunhos'
 import Top from '../components/Top'
-import Script from 'next/script'
+import Footer from '../components/Footer'
 import ContactForm from '../components/ContactForm'
+import About from '../components/About'
 
 export default function Home() {
   return (
-    <div className='pb-10'>
+    <div className=''>
         <div className="pt-8 md:h-screen px-10 bg-no-repeat bg-cover bg-center bg-[url('/background-hero-mobile.png')] xl:bg-[url('/background-hero.png')]">
           <Header />
           <Top />
         </div>
+        <About />
         <Descontos />
         <ContactForm />
+        <Footer />
         {/*<Form></Form>
         <Testemunhos></Testemunhos>*/}
     </div>
